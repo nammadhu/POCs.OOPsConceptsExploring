53820e5c962ebfcd6d23c1942530f67166b935b5
Madhusudhan V <madhusudhan.v@live.in>
2023-12-27 19:59:55 +0530

    made methods inline

--- Program.cs
+++ Program.cs
@@ -9,12 +9,24 @@
             Console.WriteLine($"Step1:ValueType");
             int a1 = 10;
             Console.WriteLine($"Calling {nameof(ChangeValue)} with {nameof(a1)}:{a1}");
+            static void ChangeValue(int x)
+                {
+                Console.WriteLine($"{nameof(ChangeValue)}: Before {x}");
+                x = 200;
+                Console.WriteLine($"{nameof(ChangeValue)}: After {x}");
+                }
             ChangeValue(a1);
             Console.WriteLine($"result={a1}");
             Console.WriteLine();
             Console.WriteLine($"Step2:ReferenceType");
             BaseClass b = new BaseClass();
             b.MyPropertyInt = 100;
+            static void ChangeReferenceType(BaseClass std2)
+                {
+                Console.WriteLine($"{nameof(ChangeReferenceType)}: Before {std2.MyPropertyInt}");
+                std2.MyPropertyInt = 200;//no need to return & assign back to main object
+                Console.WriteLine($"{nameof(ChangeReferenceType)}: After {std2.MyPropertyInt}");
+                }
             Console.WriteLine($"Calling {nameof(ChangeReferenceType)} with {nameof(b.MyPropertyInt)}:{b.MyPropertyInt}");
             ChangeReferenceType(b);//no need to get return result & assign back to main object
             Console.WriteLine($"result={b.MyPropertyInt}");
@@ -28,18 +40,8 @@
             //  b.NormalProperty2 = 1;
             Console.ReadLine();
             }
-        static void ChangeValue(int x)
-            {
-            Console.WriteLine($"{nameof(ChangeValue)}: Before {x}");
-            x = 200;
-            Console.WriteLine($"{nameof(ChangeValue)}: After {x}");
-            }
-        static void ChangeReferenceType(BaseClass std2)
-            {
-            Console.WriteLine($"{nameof(ChangeReferenceType)}: Before {std2.MyPropertyInt}");
-            std2.MyPropertyInt = 200;//no need to return & assign back to main object
-            Console.WriteLine($"{nameof(ChangeReferenceType)}: After {std2.MyPropertyInt}");
-            }
+       
+       
         }
 
     public class BaseClass : AbstractBaseClass
