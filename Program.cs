0affa7d7fa573680c1ec434bc6172350624d72d5
Madhusudhan V <madhusudhan.v@live.in>
2023-12-28 09:11:59 +0530

    Test2SizeOf

--- Program.cs
+++ Program.cs
@@ -41,26 +41,30 @@ namespace POCs.OOPsConcepTsExploring
             {
             Console.WriteLine(nameof(Test2SizeOf));
             Console.WriteLine("sizeof can be used for predefined size only,so string ,class is not possible.Instead value types are allowed at runtime");
-            Console.WriteLine($"byte occupies {sizeof(byte)} bytes ");//1
-            Console.WriteLine($"bool occupies {sizeof(bool)} bytes range {bool.TrueString} => {bool.FalseString}");//1
+            Console.WriteLine($"Everything in c@ storage is minimum of {sizeof(byte)}bytes occupation & multiples of it");//1
+            Console.WriteLine($"bool occupies {sizeof(bool)}bytes range {bool.TrueString} => {bool.FalseString}");//1
             char a0 = 'a';
             a0 = ',';
-            Console.WriteLine($"char occupies {sizeof(char)} bytes range single character 'a'-'z'-'0'-'9'-','");//2
-            Console.WriteLine($"short occupies {sizeof(short)} bytes range  {short.MinValue} => {short.MaxValue}");//4
-            Console.WriteLine($"int occupies {sizeof(int)} bytes range  {int.MinValue} => {int.MaxValue} ");//4
-            Console.WriteLine($"float occupies {sizeof(float)} bytes range  {float.MinValue} => {float.MaxValue} ");//4
-            Console.WriteLine($"long occupies {sizeof(long)} bytes range   {long.MinValue}  =>  {long.MaxValue}  ");//8
-            Console.WriteLine($"double occupies {sizeof(double)} bytes range   {double.MinValue}  =>  {double.MaxValue}  ");//8
-            Console.WriteLine($"decimal occupies {sizeof(decimal)} bytes range   {decimal.MinValue}  =>  {decimal.MaxValue}  ");//16
+            Console.WriteLine($"char occupies {sizeof(char)}bytes  because stores as 16-bit Unicode range single character 'a'-'z'-'0'-'9'-','");//2
+            Console.WriteLine($"short occupies {sizeof(short)}bytes range  {short.MinValue} => {short.MaxValue}");//4
+            Console.WriteLine($"int occupies {sizeof(int)}bytes range  {int.MinValue} => {int.MaxValue} ");//4
+            Console.WriteLine($"float occupies {sizeof(float)}bytes range  {float.MinValue} => {float.MaxValue} ");//4
+            Console.WriteLine($"long occupies {sizeof(long)}bytes range   {long.MinValue}  =>  {long.MaxValue}  ");//8
+            Console.WriteLine($"double occupies {sizeof(double)}bytes range   {double.MinValue}  =>  {double.MaxValue}  ");//8
+            Console.WriteLine($"decimal occupies {sizeof(decimal)}bytes range   {decimal.MinValue}  =>  {decimal.MaxValue}  ");//16
 
 
-            Console.WriteLine("{sizeof(string)} is not possible as 'string' does not have a predefined size");
-            //'string' does not have a predefined size, therefore sizeof can only be used in an unsafe context 
+            Console.WriteLine("{sizeof(string)} is not possible as 'string' does not have a predefined size,instead its address always stores as '4 byte address' and its content somewhere else based on its length");
+            //'string' does not have a predefined size, therefore sizeof can only be used in an unsafe context
+
+            string s1 = "s1234567";
+            Console.WriteLine($"size of string s1 {s1} is itsLength*sizeofChar(2 byte):{s1.Length * sizeof(Char)}");
 
             Console.WriteLine("{sizeof(PointStructure)} bytes now working need to check TODO");
             int size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(PointStructure));
             Console.WriteLine($"{nameof(PointStructure)} size is: {size} bytes");
 
+         
             //BaseClass b = new BaseClass() { MyPropertyInt = 12 };
             //Console.WriteLine($"{nameof(b.MyPropertyInt)} size:{Marshal.SizeOf(b)}");
             Console.WriteLine();
